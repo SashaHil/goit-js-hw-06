7e583dcaa856f9999019b9f27f6ba8e1653d7fe1
SashaHil <sashahil96@gmail.com>
2023-02-01 19:22:17 +0200

    виконав перше завдання, почав друге

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,12 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsEl = document.createElement("li");
+ingredientsEl.classList.add("item");
+// console.log(ingredientsEl);
