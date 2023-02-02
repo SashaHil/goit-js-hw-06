a9bc525ed992826159d45b3141ae79275df65557
SashaHil <sashahil96@gmail.com>
2023-02-02 14:07:53 +0200

    Виконав 2 завдання

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,7 +6,51 @@ const ingredients = [
   "Herbs",
   "Condiments",
 ];
+// 1 Спосіб
 
-const ingredientsEl = document.createElement("li");
-ingredientsEl.classList.add("item");
-// console.log(ingredientsEl);
+const list = document.querySelector("#ingredients");
+
+const firstItem = document.createElement("li");
+firstItem.textContent = "Potatoes";
+firstItem.classList.add("item");
+
+const secondItem = document.createElement("li");
+secondItem.textContent = "Mushrooms";
+secondItem.classList.add("item");
+
+const thirdItem = document.createElement("li");
+thirdItem.textContent = "Garlic";
+thirdItem.classList.add("item");
+
+const fourItem = document.createElement("li");
+fourItem.textContent = "Tomatos";
+fourItem.classList.add("item");
+
+const fiveItem = document.createElement("li");
+fiveItem.textContent = "Herbs";
+fiveItem.classList.add("item");
+
+const sixItem = document.createElement("li");
+sixItem.textContent = "Condiments";
+sixItem.classList.add("item");
+
+list.append(firstItem, secondItem, thirdItem, fourItem, fiveItem, sixItem);
+
+console.log(list);
+
+// 2 Спосіб
+
+// const list = document.querySelector("#ingredients");
+
+// const allList = [];
+
+// ingredients.forEach((ingredient) => {
+//   const itemEl = document.createElement("li");
+//   itemEl.textContent = ingredient;
+//   itemEl.classList.add("item");
+//   allList.push(itemEl);
+// });
+
+// list.append(...allList);
+
+// console.log(list);
