0aa9dbe51abda01ac62885e2a0776041d718f6f7
SashaHil <sashahil96@gmail.com>
2023-02-03 22:10:17 +0200

    Виконав 8 завдання

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,24 @@
+const form = document.querySelector(".login-form");
 
+const onFormSubmit = (event) => {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Всі поля повинні бути заповнені.");
+  }
+
+  const info = {
+    email: email.value,
+    password: password.value,
+  };
+
+  console.log(info);
+
+  form.reset();
+};
+
+form.addEventListener("submit", onFormSubmit);
