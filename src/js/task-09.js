a315f45056ddb4139e9bd82059f604c24ff7ba4b
SashaHil <sashahil96@gmail.com>
2023-02-04 22:04:15 +0200

    Виконав 9 завдання

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,17 @@
+const changeColor = document.querySelector(".change-color");
+const colorValue = document.querySelector(".color");
+const body = document.querySelector("body");
+
+const onColorChange = () => {
+  body.style.backgroundColor = getRandomHexColor();
+
+  colorValue.textContent = getRandomHexColor();
+};
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+
+changeColor.addEventListener("click", onColorChange);
